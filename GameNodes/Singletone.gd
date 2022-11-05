a4daf34c4d3de0ea70ecb885719c0f07f78b2aad
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-05 22:33:42 +0300

    Наброски доработок

--- GameNodes/Singletone.gd
+++ GameNodes/Singletone.gd
@@ -1,5 +1,29 @@
 extends Node2D
 export var Glob_pause = false
+export var Max_Helth_Player = 5 # Очевидно...
+export var Helth_Player = 1 # некое начальное значение...
+
+func check_helth():
+	if Helth_Player > Max_Helth_Player:
+		Helth_Player = Max_Helth_Player
+	if Helth_Player < 0:
+		Helth_Player = 0 # Пока так...
+		# Некое действие / гамовер...
+	
+
+func _set_helth(helth):
+	Helth_Player = helth
+
+func _set_max_helth(helth):
+	Max_Helth_Player = helth
+
+func heal(heal):
+	Helth_Player += heal
+	# Почему это отдельно он damage? Чтобы запускать анимацию в ноде Alice!
+
+func _damage(damage):
+	Helth_Player -= damage
+
 
 func _out():
 	$"../Game/Level"._out()
