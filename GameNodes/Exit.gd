b6095b7eeebbc162fb2d417beceb0e5851491e4c
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-06 02:05:39 +0300

    Починил дверь. (Ну вдруг понадобится...)

--- GameNodes/Exit.gd
+++ GameNodes/Exit.gd
@@ -2,9 +2,6 @@ extends Area2D
 
 export var next_level = "res://Scenes/sDungeon.tscn"
 
-signal get_out
-signal get_in
-
 func _ready():
 	$CollisionShape2D.disabled = true
 	var alice = get_tree().get_root().find_node("Alice",true,false)
@@ -25,6 +22,6 @@ func _on_Exit_body_exited(body):
 func _on_Interact():
 	if $Intrract.visible:
 		$CollisionShape2D.disabled = true
-		emit_signal("get_out")
-		emit_signal("get_in",next_level) #Именно тут надо загрузить следующий уровень.
+		$"/root/Singletone"._out()
+		$"/root/Singletone"._in(next_level)
 
