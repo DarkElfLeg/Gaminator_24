fd6d7b99cc64ae344d69a548be455171c9aaed23
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-06 22:46:07 +0300

    Одноразовая активность.

--- GameNodes/Activity.gd
+++ GameNodes/Activity.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export var dialog_file = "timeline-1667580737.json"
+export var visited = false
 
 func _ready():
 	var alice = get_tree().get_root().find_node("Alice",true,false)
@@ -8,19 +9,22 @@ func _ready():
 
 func _on_Interact():
 	if $Intrract.visible:
-		if not Singletone.Glob_pause:
-			Singletone.Glob_pause = true
-			$Intrract.visible = false
-			var new_dialog = Dialogic.start(dialog_file)
-			add_child(new_dialog)
-			yield(new_dialog,"tree_exited")
-			Singletone.Glob_pause = false
+		if not visited:
+			if not Singletone.Glob_pause:
+				Singletone.Glob_pause = true
+				$Intrract.visible = false
+				var new_dialog = Dialogic.start(dialog_file)
+				add_child(new_dialog)
+				yield(new_dialog,"tree_exited")
+				Singletone.Glob_pause = false
+				visited = true
 
 
 
 func _on_Activity_body_entered(body):
 	if body.name == "Alice":
-		$Intrract.visible = true
+		if not visited:
+			$Intrract.visible = true
 
 
 func _on_Activity_body_exited(body):
