206f9899e36ea7ac04c47e3515741916d1b73c4e
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-09 22:55:00 +0300

    Revert "Revert "Другое управление щитом""
    This reverts commit 0fd035455369f7cb4ffd3a9321b13572e73072f9.

--- GameNodes/Shield.gd
+++ GameNodes/Shield.gd
@@ -2,9 +2,12 @@ extends KinematicBody2D
 
 export var radius = 200
 export var speed = 0.07
+export var rotate_speed = 0.15
 var direction_speed = Vector2.ZERO
 var direct = Vector2.ZERO
 var pdir = Vector2.ZERO
+var dir_n = 0;
+
 
 func _ready():
 	var move_to = $"/root/Game/Alice".position
@@ -17,5 +20,7 @@ func _physics_process(delta):
 	var move_to = $"/root/Game/Alice".position
 	if not($"/root/Game/Alice".direction == Vector2.ZERO):
 		pdir = $"/root/Game/Alice".direction
-	direct = (move_to + pdir * radius)
+		var targetDir = get_angle_to(position - pdir.normalized())
+		rotate((targetDir)*rotate_speed)
+		direct = (move_to + Vector2(-radius,0).rotated(rotation))
 	position = lerp(position,direct,speed)
