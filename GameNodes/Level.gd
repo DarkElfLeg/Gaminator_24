f2d54ea46954939fa57a3bc117e22a3e44c0b814
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-10 16:36:00 +0100

    Проверка одного уровня дома.

--- GameNodes/Level.gd
+++ GameNodes/Level.gd
@@ -4,6 +4,7 @@ export var first_level = "res://Scenes/Cat_Scene.tscn"
 
 func _ready():
 	OS.set_window_maximized(true)
+	#OS.set_window_maximized(false)
 	#OS.set_window_size(Vector2(800,450))# Потом просто удалю. 
 	#Для работы и отладки это удобнее!
 	OS.set_window_title("Mouse Trap") 
@@ -16,6 +17,16 @@ func _in(Level):
 	var scene = load(Level)
 	var loaded = scene.instance()
 	yield(get_tree().create_timer(2.0), "timeout")
+	print("instance")
+	print(Level)
+	if Level=="res://Scenes/sHome.tscn":
+		var n = $"/root/Game/Level".get_child_count()
+		if n>1:
+			var n1=0;
+			for child in $"/root/Game/Level".get_children():
+				if n1>0:
+					child.queue_free()
+				n1+=n1
 	add_child(loaded)
 
 func _out():
