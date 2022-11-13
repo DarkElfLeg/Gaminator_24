68c5f17a117c3f9ca44c8bc0a7b0e0181caff924
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-14 00:42:54 +0300

    Проверка на переменную и покупка.
    Если это должно было зависеть от переменной.

--- GameNodes/Garbage.gd
+++ GameNodes/Garbage.gd
@@ -1,10 +1,21 @@
 extends Area2D
 
 export var dialog_file = "timeline-1667580737.json"
+export var var_name = "None"
+
 
 func _ready():
+	check_var()
 	var alice = get_tree().get_root().find_node("Alice",true,false)
 	alice.connect("Interact",self, "_on_Interact")
+
+func check_var():
+	if not var_name == "None":
+		if not Dialogic.get_variable(var_name):
+			$Intrract.visible = false
+			$CollisionPolygon2D.disabled = true
+			$"../Sprite".visible = false
+	pass
 
 func _on_Garbage_body_entered(body):
 	if body.name == "Alice":
@@ -25,13 +36,7 @@ func _on_Interact():
 			var new_dialog = Dialogic.start(dialog_file)
 			add_child(new_dialog)
 			yield(new_dialog,"tree_exited")
+			check_var()
 			Singletone.Glob_pause = false
 
 
-
-func _on_Talker_body_entered(body):
-	pass # Replace with function body.
-
-
-func _on_Talker_body_exited(body):
-	pass # Replace with function body.
