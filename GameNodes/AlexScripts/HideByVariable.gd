de4ef841151cd6ea17b115ced3a27874df594bac
alexsilent <skybros@gmail.com>
2022-11-14 15:48:51 +1000

    много коррекций уровней

--- GameNodes/AlexScripts/HideByVariable.gd
+++ GameNodes/AlexScripts/HideByVariable.gd
@@ -4,30 +4,24 @@ extends Node
 var isHided = false;
 export var childName = "";
 export var variableName = "";
-var oldVariableTest = "-";
 export var isInvert = false; # true -> Show By Variable (Not Hide)
 
 func _ready():
-	pass
-
-func _process(delta):
-	pass
+	CheckHideIt();
 	
-func CheckHide(): # Check if we Hide or Show Node by Dialogic Variable
+func CheckHideIt(): # Check if we Hide or Show Node by Dialogic Variable
 	var booleanTest = true;
-	if (oldVariableTest != Dialogic.get_variable(variableName)): #Check Update of Variable
-		oldVariableTest = Dialogic.get_variable(variableName); #set new Old Variable Number
-		
-		if (oldVariableTest == "0"):
-			booleanTest = false; #Hide Node
-		else:
-			booleanTest = true; #Show Node
+	if (Dialogic.get_variable(variableName) == "0"):
+		booleanTest = false; #Hide Node
+	else:
+		booleanTest = true; #Show Node
 			
-		if (isInvert): # Invert Hide/Show Node
-			booleanTest = not booleanTest;
+	if (isInvert): # Invert Hide/Show Node
+		booleanTest = not booleanTest;
 			
+	if (not booleanTest):
 		for child in get_children():
-			print (child.name);
 			if (child.name == childName):
-				child.visible = booleanTest;
-				child.get_node("CollisionPolygon2D").disabled = booleanTest;
+				child.queue_free();
+				#child.visible = booleanTest;
+				#child.get_node("CollisionPolygon2D").disabled = not booleanTest;
