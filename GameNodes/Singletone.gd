8b3237c8d02deaede082fcb360f7a0e1ca3dc188
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-17 16:47:46 +0100

    Думаю так пока надежнее.

--- GameNodes/Singletone.gd
+++ GameNodes/Singletone.gd
@@ -39,7 +39,7 @@ func check_helth():
 		print("Death! Go to home.")
 		$"../Game/Alice".position = Vector2(900,850)
 		yield(get_tree().create_timer(4.0), "timeout")
-		pause_off()
+		pause_off()#?! TODO!
 		Helth_Player = Max_Helth_Player
 	match Max_Helth_Player:
 		2:
