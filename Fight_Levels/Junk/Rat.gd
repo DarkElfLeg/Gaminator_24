cb77b4c21e0af8e13abab5b98b033dc1f7f6bd03
Yaroslav Pokrovskiy <46526958+DarkElfLeg@users.noreply.github.com>
2022-11-23 01:08:00 +0300

    Миниигра с крысами

--- Fight_Levels/Junk/Rat.gd
+++ Fight_Levels/Junk/Rat.gd
@@ -4,17 +4,17 @@ export var speed = 400
 var move_to = 0
 
 func _ready():
-	move_to = rand_range(100, 1820);
-	if move_to < position.x:
+	move_to = rand_range(position.x - 900, position.x + 900);
+	if move_to > position.x:
 		scale = Vector2(-1,1)
 	pass
 
 func _process(delta):
 	if not Singletone.pause_mode:
 		position.y -= speed * delta
-		if position.x < (move_to-100):
+		if position.x < (move_to+100):
 			position.x -= speed * delta
-		if position.x > (move_to+100):
+		if position.x > (move_to-100):
 			position.x += speed * delta
 		if position.y < -200:
 			queue_free()
