1753bb627305492b0c0da60e2e73ce7826877c39
alexsilent <skybros@gmail.com>
2022-11-27 12:29:20 +1000

    лучше скрипт телека

--- Fight_Levels/Junk/Spikes.gd
+++ Fight_Levels/Junk/Spikes.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 export var isDeleteByDead = true;
+var isDead = false;
+export var isRotateSpeed = 0;
 export var speed = 400;
 
 
@@ -10,6 +12,8 @@ func _ready():
 
 func _process(delta):
 	if not Singletone.pause_mode:
+		if (isDead && isRotateSpeed!=0):
+			$Sprite2.rotate(isRotateSpeed*delta);
 		position.y -= speed * delta
 		if position.y < -200:
 			queue_free()
@@ -20,6 +24,11 @@ func _on_Spikes_body_entered(body):
 	if body.name == "Alice":
 		$"/root/Game/Alice"._hert();
 		if (not isDeleteByDead):
+			isDead = true;
+			if (isRotateSpeed!=0):
+				if (rand_range(0,100) > 50): #Random Rotate Direction
+					isRotateSpeed = -isRotateSpeed; #Random Rotate Speed
+				isRotateSpeed *= rand_range(.9,1.1);
 			$Sprite.visible = false;
 			$Sprite2.visible = true;
 			$CollisionPolygon2D.disabled = true;
